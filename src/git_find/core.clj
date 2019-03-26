2404d3e7cf06136f1cce1047d255c04225fb921f
Augustine Kwanashie <akwanashie@gmail.com>
2019-03-26 21:34:17 +0000

    Move some fields to the core.clj

--- src/git_find/core.clj
+++ src/git_find/core.clj
@@ -1,2 +1,11 @@
 (ns git-find.core
+  (:require [clojure.data.json :as json])
   (:gen-class))
+
+(defn github-response-body [response]
+  (if (= 200 (:status response))
+    (json/read-str (:body response) :key-fn keyword)
+    (throw (Exception. "Error"))))
+
+(defn repo-details [repo]
+  {:name (:name repo)})
--- src/git_find/shell.clj
+++ src/git_find/shell.clj
@@ -1,7 +1,7 @@
 (ns git-find.shell
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]
-            [environ.core :refer [env]])
+            [environ.core :refer [env]]
+            [git-find.core :as core])
   (:gen-class))
 
 (def github-url "https://api.github.com")
@@ -13,19 +13,11 @@
     (client/get full-url {:headers {:Authorization token}
                           :query-params query-params})))
 
-(defn github-response-body [response]
-  (if (= 200 (:status response))
-    (json/read-str (:body response) :key-fn keyword)
-    (throw (Exception. "Error"))))
-
-(defn repo-details [repo]
-  {:name (:name repo)})
-
 (defn github-repos [team-name]
   (-> "/user/repos"
       (github-request {:type "owner"})
-      (github-response-body)
-      ((fn [repos] (map repo-details repos)))))
+      (core/github-response-body)
+      ((fn [repos] (map core/repo-details repos)))))
 
 (defn -main []
   (-> "connections"
