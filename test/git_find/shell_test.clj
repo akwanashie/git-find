381777c20fa27a53d0097715455c315361e3ff2a
Augustine Kwanashie <akwanashie@gmail.com>
2019-03-27 16:00:04 +0000

    Test functions with side effects

--- test/git_find/shell_test.clj
+++ test/git_find/shell_test.clj
@@ -1,3 +1,14 @@
 (ns git-find.shell-test
   (:require [clojure.test :refer :all]
+            [clj-http.client :as client]
             [git-find.shell :refer :all]))
+
+(deftest test-github-get
+  (testing "Should call github with the correct url"
+    (let [expected-url "https://api.github.com/user/repos"
+          error-message #(str "Wrong Url: expected " expected-url " but found " %)
+          mock-fn (fn [url options] (if (= expected-url url)
+                                      {:status 200}
+                                      (throw (Exception. (error-message url)))))]
+      (with-redefs-fn {#'client/get mock-fn}
+        #(is (github-get "/user/repos" {:type "owner"}))))))
